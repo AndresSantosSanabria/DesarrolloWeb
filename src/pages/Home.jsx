51bbf75f9576e3d8ca00c98b3fea39fad0ec62eb
DayanaBaron <sarmientoangie626@gmail.com>
2023-10-19 18:16:10 -0500

    SliderHome terminado

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import Bloque from "../components/Bloques/Bloque"
 import GaleriaHover from "../components/GaleriaHover/GaleriaHover";
+import SliderHome from "../components/SliderHome/SliderHome";
 
 function Home() {
     return (
         <>
+        <SliderHome></SliderHome>
         <GaleriaHover></GaleriaHover>
         <Bloque></Bloque>
         </>
